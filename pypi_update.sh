4128d6f1eedd6f11bd86e049e9da0e96383c8bef
dai-motoki <dai.motoki1123@gmail.com>
2024-06-06 13:54:21 +0900

    Release v1.2.6

--- pypi_update.sh
+++ pypi_update.sh
@@ -1,4 +1,4 @@
-pip install wheel
+# pip install wheel
 python setup.py sdist bdist_wheel
-twine upload dist/grimo-1.2.5*
-git add . && git commit -m "Release v1.2.5" && git push && git tag v1.2.5 && git push --tags
+twine upload dist/grimo-1.2.6*
+# git add . && git commit -m "Release v1.2.6" && git push && git tag v1.2.6 && git push --tags
