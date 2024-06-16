1401dc2d9f6faeb0b4c1f068682b09cdee0b2113
dai-motoki <dai.motoki1123@gmail.com>
2024-06-16 13:54:02 +0900

    Release v1.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grimo',
-    version='1.3.3',
+    version='1.3.4',
     description='Abstract Programming Language Package Manager',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',  # 追加: long_descriptionの形式を指定
@@ -16,6 +16,7 @@ setup(
         'colorama',
         'boto3',
         'streamlit',
+        'aiofiles',
         'i18nice[YAML]',
     ],
     entry_points={
