a7d9e0a9f84b7fca7af35279cd40351bbd05368e
dai-motoki <dai.motoki1123@gmail.com>
2024-06-19 02:21:15 +0900

    Release v1.3.11

--- pypi_update.sh
+++ pypi_update.sh
@@ -1,4 +1,4 @@
 # pip install wheel
 python setup.py sdist bdist_wheel
-twine upload dist/grimo-1.3.10*
-git add . && git commit -m "Release v1.3.10" && git push && git tag v1.3.10 && git push --tags
+twine upload dist/grimo-1.3.11*
+git add . && git commit -m "Release v1.3.11" && git push && git tag v1.3.11 && git push --tags
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grimo',
-    version='1.3.10',
+    version='1.3.11',
     description='Abstract Programming Language Package Manager',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',  # 追加: long_descriptionの形式を指定
