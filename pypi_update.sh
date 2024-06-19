17d067c39f4c5ebd14de2fe7f913497d453b332a
dai-motoki <dai.motoki1123@gmail.com>
2024-06-19 11:48:13 +0900

    Release v1.3.40

--- pypi_update.sh
+++ pypi_update.sh
@@ -1,4 +1,4 @@
 # pip install wheel
 python setup.py sdist bdist_wheel
-twine upload dist/grimo-1.3.39*
-git add . && git commit -m "Release v1.3.39" && git push && git tag v1.3.39 && git push --tags
+twine upload dist/grimo-1.3.40*
+git add . && git commit -m "Release v1.3.40" && git push && git tag v1.3.40 && git push --tags
